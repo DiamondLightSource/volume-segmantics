1863fb73393287a70e3b803df5b4bdeaae302022
Olly King <olly.king@diamond.ac.uk>
2020-11-26 16:46:06 +0000

    First working version of object orientated 2d unet training

---
+++ 2dunet/scripts/utilities/data.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+"""Data utilities for U-net training and prediction.
+"""
+import glob
+import logging
+import os
+import sys
+from pathlib import Path
+import warnings
+
+import dask.array as da
+import h5py as h5
+import numpy as np
+import yaml
+from skimage import exposure, img_as_ubyte, io
+from tqdm import tqdm
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+class SettingsData:
+    """Class to sanity check and then store settings from the commandline and
+    a YAML settings file. Assumes given commandline args are filepaths.
+
+    Args:
+        settings_path (pathlib.Path): Path to the YAML file containing user settings.
+        parser_args (argparse.Namespace): Parsed commandline arguments from argparse.
+    """
+    def __init__(self, settings_path, parser_args):
+        logging.info(f"Loading settings from {settings_path}")
+        if settings_path.exists():
+            self.settings_path = settings_path
+            with open(settings_path, 'r') as stream:
+                self.settings_dict = yaml.safe_load(stream)
+        else:
+            logging.error("Couldn't find settings file... Exiting!")
+            sys.exit(1)
+        logging.debug(f"Commandline args given: {vars(parser_args)}")
+
+        # Set the data as attributes, check paths are valid files
+        for k, v in self.settings_dict.items():
+            setattr(self, k, v)
+        for k, v in vars(parser_args).items():
+            # Check that files exist
+            v = Path(v)
+            if v.is_file():
+                setattr(self, k, v)
+            else:
+                logging.error(f"The file {v} does not appear to exist. Exiting!")
+                sys.exit(1)
+
+
+class DataSlicer:
+    """Class that converts 3d data volumes into 2d image slices on disk.
+    Slicing is carried in one or all of the xy (z), xz (y) and yz (x) planes.
+
+    Args:
+        settings (SettingsData): An initialised SettingsData object.
+    """
+
+    def __init__(self, settings):
+        self.multilabel = False
+        self.data_im_out_dir = None
+        self.seg_im_out_dir = None
+        self.data_vol = self.da_from_data(settings.data_vol_path)
+        self.seg_vol = self.da_from_data(settings.seg_vol_path)
+        self.st_dev_factor = settings.st_dev_factor
+        if settings.normalise:
+            self.data_vol = self.clip_to_uint8(self.data_vol.compute())
+        seg_classes = np.unique(self.seg_vol.compute())
+        self.num_seg_classes = len(seg_classes)
+        if self.num_seg_classes > 2:
+            self.multilabel = True
+        logging.info("Number of classes in segmentation dataset:"
+                     f" {self.num_seg_classes}")
+        logging.info(f"These classes are: {seg_classes}")
+        if seg_classes[0] != 0:
+            logging.info("Fixing label classes.")
+            self.fix_label_classes(seg_classes)
+        self.codes = [f"label_val_{i}" for i in seg_classes]
+
+    def da_from_data(self, path):
+        """Returns a dask array when given a path to an HDF5 file.
+
+        The data is assumed to be found in '/data' in the file.
+
+        Args:
+            path(pathlib.Path): The path to the HDF5 file.
+
+        Returns:
+            dask.array: A dask array object for the data stored in the HDF5 file.
+        """
+        f = h5.File(path, 'r')
+        d = f['/data']
+        return da.from_array(d, chunks='auto')
+
+    def clip_to_uint8(self, data):
+        """Clips data to a certain number of st_devs of the mean and reduces
+        bit depth to uint8.
+
+        Args:
+            data(np.array): The data to be processed.
+
+        Returns:
+            np.array: A unit8 data array.
+        """
+        logging.info("Clipping data and converting to uint8.")
+        data_st_dev = np.std(data)
+        data_mean = np.mean(data)
+        num_vox = np.prod(data.shape)
+        lower_bound = data_mean - (data_st_dev * self.st_dev_factor)
+        upper_bound = data_mean + (data_st_dev * self.st_dev_factor)
+        gt_ub = (data > upper_bound).sum()
+        lt_lb = (data < lower_bound).sum()
+        logging.info(f"Lower bound: {lower_bound}, upper bound: {upper_bound}")
+        logging.info(
+            f"Number of voxels above upper bound to be clipped {gt_ub} - percentage {gt_ub/num_vox * 100:.3f}%")
+        logging.info(
+            f"Number of voxels below lower bound to be clipped {lt_lb} - percentage {lt_lb/num_vox * 100:.3f}%")
+        data = np.clip(data, lower_bound, upper_bound)
+        data = exposure.rescale_intensity(data, out_range='float')
+        return img_as_ubyte(data)
+
+    def fix_label_classes(self, seg_classes):
+        """Changes the data values of classes in a segmented volume so that
+        they start from zero.
+
+        Args:
+            seg_classes(list): An ascending list of the labels in the volume.
+        """
+        if isinstance(self.seg_vol, da.core.Array):
+            self.seg_vol = self.seg_vol.compute()
+        for idx, current in enumerate(seg_classes):
+            self.seg_vol[self.seg_vol == current] = idx
+
+    def output_data_slices(self, data_dir, axis):
+        """Wrapper method to intitiate slicing data volume to disk.
+
+        Args:
+            data_dir (pathlib.Path): The path to the direcotry where images will be saved.
+            axis (str): One of [all, x, y, or z]. Which planes to slice.
+        """
+        self.data_im_out_dir = data_dir
+        logging.info(
+            'Slicing data volume and saving slices to disk')
+        os.makedirs(data_dir, exist_ok=True)
+        self.output_slices_to_disk(axis, self.data_vol, data_dir, 'data')
+
+    def output_label_slices(self, data_dir, axis):
+        """Wrapper method to intitiate slicing label volume to disk.
+
+        Args:
+            data_dir (pathlib.Path): The path to the direcotry where images will be saved.
+            axis (str): One of [all, x, y, or z]. Which planes to slice.
+        """
+        self.seg_im_out_dir = data_dir
+        logging.info(
+            'Slicing label volume and saving slices to disk')
+        os.makedirs(data_dir, exist_ok=True)
+        self.output_slices_to_disk(
+            axis, self.seg_vol, data_dir, 'seg', label=True)
+
+    def output_slices_to_disk(self, axis, data_arr, output_path, name_prefix, label=False):
+        """Coordinates the slicing of an image volume in one or all of the three orthogonal
+        planes to images on disk. 
+        
+        Args:
+            axis (str): Which plane to slice the data in. Either 'x', 'y, 'z' or 'all'.
+            data_arr (array): The data volume to be sliced.
+            output_path (pathlib.Path): A Path object to the output directory.
+            label (bool): Whether this is a label volume.
+        """
+        shape_tup = data_arr.shape
+        # There has to be a cleverer way to do this!
+        if axis in ['z', 'all']:
+            logging.info('Outputting z stack')
+            for val in tqdm(range(shape_tup[0])):
+                out_path = output_path/f"{name_prefix}_z_stack_{val}"
+                self.output_im(data_arr[val, :, :], out_path, label)
+        if axis in ['y', 'all']:
+            logging.info('Outputting y stack')
+            for val in tqdm(range(shape_tup[1])):
+                out_path = output_path/f"{name_prefix}_y_stack_{val}"
+                self.output_im(data_arr[:, val, :], out_path, label)
+        if axis in ['x', 'all']:
+            logging.info('Outputting x stack')
+            for val in tqdm(range(shape_tup[2])):
+                out_path = output_path/f"{name_prefix}_x_stack_{val}"
+                self.output_im(data_arr[:, :, val], out_path, label)
+        if axis not in ['x', 'y', 'z', 'all']:
+            logging.error("Axis should be one of: [all, x, y, or z]!")
+
+    def output_im(self, data, path, label=False):
+        """Converts a slice of data into an image on disk.
+    
+        Args:
+            data (numpy.array): The data slice to be converted.
+            path (str): The path of the image file including the filename prefix.
+            label (bool): Whether to convert values >1 to 1 for binary segmentation.
+        """
+        if isinstance(data, da.core.Array):
+            data = data.compute()
+        if label and not self.multilabel:
+            data[data > 1] = 1
+        io.imsave(f'{path}.png', data)
+
+    def delete_data_im_slices(self):
+        """Deletes image slices in the data image output directory. Leaves the
+        directory in place since it contains model training history.
+        """
+        if self.data_im_out_dir:
+            data_ims = glob.glob(f"{str(self.data_im_out_dir) + '/*.png'}")
+            logging.info(f"Deleting {len(data_ims)} image slices")
+            for fn in data_ims:
+                os.remove(fn)
+
+    def delete_label_im_slices(self):
+        """Deletes label image slices in the segmented image output directory.
+        Also deletes the directory itself.
+        """
+        if self.seg_im_out_dir:
+            seg_ims = glob.glob(f"{str(self.seg_im_out_dir) + '/*.png'}")
+            logging.info(f"Deleting {len(seg_ims)} segmentation slices")
+            for fn in seg_ims:
+                os.remove(fn)
+            logging.info(f"Deleting the empty segmentation image directory")
+            os.rmdir(self.seg_im_out_dir)
+
+    def clean_up_slices(self):
+        """Wrapper function that cleans up data and label image slices.
+        """
+        self.delete_data_im_slices()
+        self.delete_label_im_slices()
---
+++ 2dunet/scripts/utilities/unet2d.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+"""Classes for 2d U-net training and prediction.
+"""
+import logging
+import sys
+from functools import partial
+from pathlib import Path
+
+import numpy as np
+import torch.nn.functional as F
+from fastai.callbacks import CSVLogger, SaveModelCallback
+from fastai.utils.mem import gpu_mem_get_free_no_cache
+from fastai.vision import (SegmentationItemList, dice, get_transforms,
+                           imagenet_stats, lr_find, models, open_image,
+                           unet_learner)
+from matplotlib import pyplot as plt
+from skimage import exposure, img_as_ubyte, io
+from tqdm import tqdm
+
+
+class Unet2dTrainer:
+    """Class that takes in 2d images and corresponding segmentations and
+    trains a 2dUnet with a pretrained ResNet34 encoder.
+
+    Args:
+        data_im_out_dir (pathlib.Path): Path to directory containing image slices.
+        seg_im_out_dir (pathlib.Path): Path to to directory containing label slices.
+        codes (list of str): Names of the label classes, must be the same length as
+        number of classes.
+        im_size (int): Size of images to input to network.
+        weight_decay (float): Value of the weight decay regularisation term to use.
+    """
+
+    def __init__(self, data_im_out_dir, seg_im_out_dir, codes, settings):
+        self.data_dir = data_im_out_dir
+        self.label_dir = seg_im_out_dir
+        self.codes = codes
+        self.multilabel = len(codes) > 2
+        self.image_size = settings.image_size
+        # Params for learning rate finder
+        self.lr_find_lr_diff = 15
+        self.lr_find_loss_threshold = 0.05
+        self.lr_find_adjust_value = 1
+        # Params for model training
+        self.weight_decay = float(settings.weight_decay)
+        self.num_cyc_frozen = settings.num_cyc_frozen
+        self.num_cyc_unfrozen = settings.num_cyc_unfrozen
+        self.pct_lr_inc = settings.pct_lr_inc
+        # Set up model ready for training
+        self.batch_size = self.get_batchsize()
+        self.create_training_dataset()
+        self.setup_metrics_and_loss()
+        self.create_model()
+
+    def setup_metrics_and_loss(self):
+        """Sets instance attributes for loss function and evaluation metrics
+        according to whether binary or multilabel segmentation is being
+        performed. 
+        """
+        if self.multilabel:
+            logging.info("Setting up for multilabel segmentation since there are "
+                         f"{len(self.codes)} classes")
+            self.metrics = self.accuracy
+            self.monitor = 'accuracy'
+            self.loss_func = None
+        else:
+            logging.info("Setting up for binary segmentation since there are "
+                         f"{len(self.codes)} classes")
+            self.metrics = [partial(dice, iou=True)]
+            self.monitor = 'dice'
+            self.loss_func = self.bce_loss
+
+    def create_training_dataset(self):
+        """Creates a fastai segmentation dataset and stores it as an instance
+        attribute.
+        """
+        logging.info("Creating training dataset from saved images.")
+        src = (SegmentationItemList.from_folder(self.data_dir)
+               .split_by_rand_pct()
+               .label_from_func(self.get_label_name, classes=self.codes))
+        self.data = (src.transform(get_transforms(), size=self.image_size, tfm_y=True)
+                     .databunch(bs=self.batch_size)
+                     .normalize(imagenet_stats))
+
+    def create_model(self):
+        """Creates a deep learning model linked to the dataset and stores it as
+        an instance attribute.
+        """
+        logging.info("Creating 2d U-net model for training.")
+        self.model = unet_learner(self.data, models.resnet34, metrics=self.metrics,
+                                  wd=self.weight_decay, loss_func=self.loss_func,
+                                  callback_fns=[partial(CSVLogger,
+                                                filename='unet_training_history',
+                                                append=True),
+                                                partial(SaveModelCallback,
+                                                monitor=self.monitor, mode='max',
+                                                name="best_unet_model")])
+
+    def train_model(self):
+        """Performs transfer learning training of model for a number of cycles
+        with parameters frozen or unfrozen and a learning rate that is determined automatically.
+        """
+        if self.num_cyc_frozen > 0:
+            logging.info("Finding learning rate for frozen Unet model.")
+            lr_to_use = self.find_appropriate_lr()
+            logging.info(
+                f"Training frozen Unet for {self.num_cyc_frozen} cycles with learning rate of {lr_to_use}.")
+            self.model.fit_one_cycle(self.num_cyc_frozen, slice(
+                lr_to_use/50, lr_to_use), pct_start=self.pct_lr_inc)
+        if self.num_cyc_unfrozen > 0:
+            self.model.unfreeze()
+            logging.info("Finding learning rate for unfrozen Unet model.")
+            lr_to_use = self.find_appropriate_lr()
+            logging.info(
+                f"Training unfrozen Unet for {self.num_cyc_unfrozen} cycles with learning rate of {lr_to_use}.")
+            self.model.fit_one_cycle(self.num_cyc_unfrozen, slice(
+                lr_to_use/50, lr_to_use), pct_start=self.pct_lr_inc)
+
+    def save_model_weights(self, model_filepath):
+        """Saves the model weights to a specified location.
+
+        Args:
+            model_filepath (pathlib.Path): Full path to location to save model
+            weights excluding file extension.
+        """
+        logging.info(f"Saving the model weights to: {model_filepath}")
+        self.model.save(model_filepath)
+
+    def output_prediction_figure(self, model_path):
+        """Saves a figure containing image slice data for three random images
+        fromthe validation dataset along with the corresponding ground truth
+        label image and corresponding prediction output from the model attached
+        to this class instance. The image is saved to the same directory as the
+        model weights. 
+
+        Args:
+            model_path (pathlib.Path): Full path to the model weights file,
+            this is used to get the directory and name of the model not to 
+            load and predict.
+        """
+        # Remove the restriction on the model prediction size
+        self.model.data.single_ds.tfmargs['size'] = None
+        filename_list = self.data.valid_ds.items[:3]
+        img_list = []
+        pred_list = []
+        gt_list = []
+        for fn in filename_list:
+            img_list.append(open_image(fn))
+            gt_list.append(io.imread(self.get_label_name(fn)))
+        for img in img_list:
+            pred_list.append(img_as_ubyte(self.model.predict(img)[1][0]))
+        # Horrible conversion from Fastai image to unit8 data array
+        img_list = [img_as_ubyte(exposure.rescale_intensity(
+            x.data.numpy()[0, :, :])) for x in img_list]
+
+        # Create the plot
+        fig = plt.figure(figsize=(12, 12))
+        columns = 3
+        rows = 3
+        j = 0
+        for i in range(columns*rows)[::3]:
+            img = img_list[j]
+            gt = gt_list[j]
+            pred = pred_list[j]
+            col1 = fig.add_subplot(rows, columns, i + 1)
+            plt.imshow(img, cmap='gray')
+            col2 = fig.add_subplot(rows, columns, i + 2)
+            plt.imshow(gt, cmap='gray')
+            col3 = fig.add_subplot(rows, columns, i + 3)
+            plt.imshow(pred, cmap='gray')
+            j += 1
+            if i == 0:
+                col1.title.set_text('Data')
+                col2.title.set_text('Ground Truth')
+                col3.title.set_text('Prediction')
+        plt.suptitle(f"Predictions for {model_path.name}", fontsize=16)
+        plt_out_pth = model_path.parent/f'{model_path.stem}_prediction_image.png'
+        logging.info(f"Saving example image predictions to {plt_out_pth}")
+        plt.savefig(plt_out_pth, dpi=300)
+
+    def find_appropriate_lr(self):
+        """Function taken from https://forums.fast.ai/t/automated-learning-rate-suggester/44199
+        which attempts to automatically find a learning rate from the fastai lr_find function.
+            
+            Returns:
+                float: A value for a sensible learning rate to use for training.
+
+        """
+        lr_find(self.model)
+        #Get loss values and their corresponding gradients, and get lr values
+        losses = np.array(self.model.recorder.losses)
+        assert(self.lr_find_lr_diff < len(losses))
+        loss_grad = np.gradient(losses)
+        learning_rates = self.model.recorder.lrs
+
+        #Search for index in gradients where loss is lowest before the loss spike
+        #Initialize right and left idx using the lr_diff as a spacing unit
+        #Set the local min lr as -1 to signify if threshold is too low
+        local_min_lr = 0.001  # Add as default value to fix bug
+        r_idx = -1
+        l_idx = r_idx - self.lr_find_lr_diff
+        while (l_idx >= -len(losses)) and (abs(loss_grad[r_idx] - loss_grad[l_idx])
+               > self.lr_find_loss_threshold):
+            local_min_lr = learning_rates[l_idx]
+            r_idx -= 1
+            l_idx -= 1
+
+        lr_to_use = local_min_lr * self.lr_find_adjust_value
+        return lr_to_use
+
+    def get_batchsize(self):
+        """Provides an appropriate batch size based upon free GPU memory. 
+
+        Returns:
+            int: A batch size for model training.
+        """
+        gpu_free_mem = gpu_mem_get_free_no_cache()
+        if gpu_free_mem > 8200:
+            batch_size = 8
+        else:
+            batch_size = 4
+        logging.info(f"Using batch size of {batch_size}, have {gpu_free_mem} MB" \
+            " of GPU RAM free.")
+        return batch_size
+
+    def bce_loss(self, logits, labels):
+        """Function to calulate Binary Cross Entropy loss from predictions.
+
+        Args:
+            logits (torch.Tensor): output from network.
+            labels (torch.Tensor): ground truth label values.
+
+        Returns:
+            torch.Tensor: The BCE loss calulated on the predictions.
+        """
+        logits = logits[:, 1, :, :].float()
+        labels = labels.squeeze(1).float()
+        return F.binary_cross_entropy_with_logits(logits, labels)
+    
+    def accuracy(self, input, target):
+        """Calculates and accuracy metric between predictions and ground truth
+        labels.
+
+        Args:
+            input (torch.Tensor): The predictions.
+            target (torchTensor): The desired output (ground truth).
+
+        Returns:
+            [type]: [description]
+        """
+        target = target.squeeze(1)
+        return (input.argmax(dim=1) == target).float().mean()
+ 
+    def get_label_name(self, img_fname):
+        """Converts a path fo an image slice to a path for corresponding label
+        slice.
+
+        Args:
+            img_fname (pathlib.Path): Path to an image slice file.
+
+        Returns:
+            pathlib.Path: Path to the corresponding segmentation label slice file. 
+        """
+        return self.label_dir/f'{"seg" + img_fname.stem[4:]}{img_fname.suffix}'
