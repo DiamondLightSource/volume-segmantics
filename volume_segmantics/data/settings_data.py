e65022794aa5b90d3da6bae1beeb33af949106d3
Olly King <olly.king@diamond.ac.uk>
2022-06-20 15:39:21 +0100

    Allow settings data object to be created from a dictionary

--- volume_segmantics/data/settings_data.py
+++ volume_segmantics/data/settings_data.py
@@ -7,19 +7,20 @@ from typing import Union
 import yaml
 
 
-def get_settings_data(path: Union[Path, None]) -> SimpleNamespace:
-    """Creates an object to hold settings data. If a path to to a YAML file,
-    the settings are read in from that file. If the path is None, an empty namespace
-    is returned.
+def get_settings_data(data: Union[Path, dict, None]) -> SimpleNamespace:
+    """Creates an object holding settings data if a path to a YAML file or a
+    dictionary is given. If the data is None, an empty namespace is returned.
     """
-    if path is None:
+    if data is None:
         return SimpleNamespace()
-    else:
-        logging.info(f"Loading settings from {path}")
-        if path.exists():
-            with open(path, "r") as stream:
+    elif isinstance(data, Path):
+        logging.info(f"Loading settings from {data}")
+        if data.exists():
+            with open(data, "r") as stream:
                 settings_dict = yaml.safe_load(stream)
             return SimpleNamespace(**settings_dict)
         else:
             logging.error("Couldn't find settings file... Exiting!")
             sys.exit(1)
+    elif isinstance(data, dict):
+        return SimpleNamespace(**data)
