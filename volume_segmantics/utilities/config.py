6d02e7f78296906b8a42b9d8cc30273e2d0590d7
Olly King <olly.king@diamond.ac.uk>
2022-07-14 14:24:26 +0100

    Reduces batch sizes to deal with larger image sizes

--- volume_segmantics/utilities/config.py
+++ volume_segmantics/utilities/config.py
@@ -28,8 +28,8 @@ HDF5_COMPRESSION = "gzip"
 
 BIG_CUDA_THRESHOLD = 8 # GPU Memory (GB), above this value batch size is increased
 BIG_CUDA_TRAIN_BATCH = 12 # Size of training batch on big GPU
-BIG_CUDA_PRED_BATCH = 12 # Size of prediction batch on big GPU
-SMALL_CUDA_BATCH = 4 # Size of batch on small GPU
+BIG_CUDA_PRED_BATCH = 4 # Size of prediction batch on big GPU
+SMALL_CUDA_BATCH = 2 # Size of batch on small GPU
 NUM_WORKERS = 4 # Number of parallel workers for training/validation dataloaders
 PIN_CUDA_MEMORY = True # Whether to pin CUDA memory for faster data transfer
 IM_SIZE_DIVISOR = 32 # Image dimensions need to be a multiple of this value
