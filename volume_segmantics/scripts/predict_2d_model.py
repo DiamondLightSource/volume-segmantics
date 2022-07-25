17077731199b22a3849742f4827e848d2b831f61
Olly King <olly.king@diamond.ac.uk>
2022-07-25 12:02:02 +0100

    Simplifies API for segmentation prediction
    - Predictor object no longer needs to be instantiated
    - **API-BREAKING CHANGE**

--- volume_segmantics/scripts/predict_2d_model.py
+++ volume_segmantics/scripts/predict_2d_model.py
@@ -7,8 +7,7 @@ from pathlib import Path
 
 import volume_segmantics.utilities.config as cfg
 from volume_segmantics.data import get_settings_data
-from volume_segmantics.model import (VolSeg2DPredictionManager,
-                                     VolSeg2dPredictor)
+from volume_segmantics.model import VolSeg2DPredictionManager
 from volume_segmantics.utilities import get_2d_prediction_parser
 
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -23,16 +22,19 @@ def main():
     logging.basicConfig(
         level=logging.INFO, format=cfg.LOGGING_FMT, datefmt=cfg.LOGGING_DATE_FMT
     )
+    # Parse Args
     parser = get_2d_prediction_parser()
     args = parser.parse_args()
+    # Define paths
     root_path = Path(getattr(args, cfg.DATA_DIR_ARG)).resolve()
     settings_path = Path(root_path, cfg.SETTINGS_DIR, cfg.PREDICTION_SETTINGS_FN)
-    settings = get_settings_data(settings_path)
     model_file_path = getattr(args, cfg.MODEL_PTH_ARG)
-    predictor = VolSeg2dPredictor(model_file_path, settings)
     data_vol_path = Path(getattr(args, cfg.PREDICT_DATA_ARG))
     output_path = create_output_path(root_path, data_vol_path)
-    pred_manager = VolSeg2DPredictionManager(predictor, data_vol_path, settings)
+    # Get settings object
+    settings = get_settings_data(settings_path)
+    # Create prediction manager and predict
+    pred_manager = VolSeg2DPredictionManager(model_file_path, data_vol_path, settings)
     pred_manager.predict_volume_to_path(output_path)
 
 
